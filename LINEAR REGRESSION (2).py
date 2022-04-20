34aca0473333493f4ea3e8a2beb935b6224a6af6
ramjipalanivel <34581698+ramjipalanivel@users.noreply.github.com>
2022-04-20 15:53:29 +0530

    Add files via upload

---
+++ LINEAR REGRESSION (2).py
@@ -0,0 +1,53 @@
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+
+dataset = pd.read_csv('student_scores.csv')  
+dataset.shape  
+
+dataset.head() 
+
+dataset.describe()
+
+
+
+
+dataset.plot(x='Hours', y='Scores', style='o')  
+plt.title('Hours vs Percentage')  
+plt.xlabel('Hours Studied')  
+plt.ylabel('Percentage Score')  
+plt.show()  
+
+
+
+X = dataset.iloc[:, :-1].values  
+y = dataset.iloc[:, 1].values  
+
+from sklearn.model_selection import train_test_split  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+                                                    random_state=0)  
+
+
+from sklearn.linear_model import LinearRegression  
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train) 
+
+print(regressor.intercept_)  
+
+
+
+print(regressor.coef_)  
+
+#Prints R Square value
+regressor.score(X_train, y_train) 
+y_pred = regressor.predict(X_test)  
+
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})  
+df['Residual'] = df['Actual'] - df['Predicted']
+df
+
+from sklearn import metrics  
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))  
